b5e8da3a18f515813cf5c8e6deee96bc6c7583f9
Krish Sahni <ksahni1_be24@thapar.edu>
2025-11-13 10:41:11 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,31 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+bool isBSTUtil(Node* root, int minVal, int maxVal) {
+    if (!root) return true;
+    if (root->data <= minVal || root->data >= maxVal) return false;
+    return isBSTUtil(root->left, minVal, root->data) && isBSTUtil(root->right, root->data, maxVal);
+}
+
+bool isBST(Node* root) {
+    return isBSTUtil(root, INT_MIN, INT_MAX);
+}
+
+int main() {
+    Node* root = new Node(50);
+    root->left = new Node(30);
+    root->right = new Node(70);
+    root->left->left = new Node(20);
+    root->left->right = new Node(40);
+    root->right->left = new Node(60);
+    root->right->right = new Node(80);
+
+    cout << (isBST(root) ? "Yes" : "No") << endl;
+}
